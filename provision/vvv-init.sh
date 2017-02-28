05196ccc3163634b978fe5b810b3a9d2c87b9a3b
Jeremy Felt <jeremy.felt@gmail.com>
2017-02-28 08:34:37 -0800

    Update VVV config to support GitHub dev repo

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,9 +1,9 @@
 # Provision WordPress Develop
 
 # Make a database, if we don't already have one
-echo -e "\nCreating database 'wordpress_develop' (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS wordpress_develop"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON wordpress_develop.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\nCreating database 'wordpress_gh' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS wordpress_gh"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON wordpress_gh.* TO wp@localhost IDENTIFIED BY 'wp';"
 echo -e "\n DB operations done.\n\n"
 
 # Nginx Logs
@@ -16,62 +16,30 @@ touch ${VVV_PATH_TO_SITE}/log/build.access.log
 # Checkout, install and configure WordPress trunk via develop.svn
 if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
   echo "Checking out WordPress trunk. See https://develop.svn.wordpress.org/trunk"
-  noroot svn checkout "https://develop.svn.wordpress.org/trunk/" "/tmp/wordpress-develop"
-
-  cd /tmp/wordpress-develop/src/
-
-  echo "Installing local npm packages for src.wordpress-develop.dev, this may take several minutes."
-  noroot npm install
-
-  echo "Initializing grunt and creating build.wordpress-develop.dev, this may take several minutes."
-  noroot grunt
+  noroot git clone https://github.com/jeremyfelt/wordpress-develop.git /tmp/wordpress-gh
 
   echo "Moving WordPress develop to a shared directory, ${VVV_PATH_TO_SITE}/public_html"
-  mv /tmp/wordpress-develop ${VVV_PATH_TO_SITE}/public_html
+  mv /tmp/wordpress-gh ${VVV_PATH_TO_SITE}/public_html
 
   cd ${VVV_PATH_TO_SITE}/public_html/src/
-  echo "Creating wp-config.php for src.wordpress-develop.dev and build.wordpress-develop.dev."
-  noroot wp core config --dbname=wordpress_develop --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+  echo "Creating wp-config.php for gh.wordpress-develop.dev."
+  noroot wp core config --dbname=wordpress_gh --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
 // Match any requests made via xip.io.
-if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(src|build)(.wordpress-develop.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
+if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(gh)(.wordpress-develop.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
     define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
     define( 'WP_SITEURL', 'http://' . \$_SERVER['HTTP_HOST'] );
 } else if ( 'build' === basename( dirname( __FILE__ ) ) ) {
-// Allow (src|build).wordpress-develop.dev to share the same Database
-    define( 'WP_HOME', 'http://build.wordpress-develop.dev' );
-    define( 'WP_SITEURL', 'http://build.wordpress-develop.dev' );
+// Allow (gh).wordpress-develop.dev to share the same Database
+    define( 'WP_HOME', 'http://gh.wordpress-develop.dev' );
+    define( 'WP_SITEURL', 'http://gh.wordpress-develop.dev' );
 }
 
 define( 'WP_DEBUG', true );
 PHP
 
-  echo "Installing src.wordpress-develop.dev."
-  noroot wp core install --url=src.wordpress-develop.dev --quiet --title="WordPress Develop" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
+  echo "Installing gh.wordpress-develop.dev."
+  noroot wp core install --url=gh.wordpress-develop.dev --quiet --title="WordPress Develop (GH)" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
   cp /srv/config/wordpress-config/wp-tests-config.php ${VVV_PATH_TO_SITE}/public_html/
   cd ${VVV_PATH_TO_SITE}/public_html/
 
-else
-
-  echo "Updating WordPress develop..."
-  cd ${VVV_PATH_TO_SITE}/public_html/
-  if [[ -e .svn ]]; then
-    svn up
-  else
-
-    if [[ $(git rev-parse --abbrev-ref HEAD) == 'master' ]]; then
-      git pull --no-edit git://develop.git.wordpress.org/ master
-    else
-      echo "Skip auto git pull on develop.git.wordpress.org since not on master branch"
-    fi
-
-  fi
-
-  echo "Updating npm packages..."
-  noroot npm install &>/dev/null
-fi
-
-if [[ ! -d "${VVV_PATH_TO_SITE}/public_html/build" ]]; then
-  echo "Initializing grunt in WordPress develop... This may take a few moments."
-  cd ${VVV_PATH_TO_SITE}/public_html/
-  grunt
 fi
